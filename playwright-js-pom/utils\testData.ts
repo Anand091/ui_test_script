421945ad7030aad8879383d31727c84dc0f436c2
Anand091 <anandkummari8765@gmail.com>
2025-07-30 01:45:02 +0530

    Add generated Playwright POM test suite

--- playwright-js-pom/utils\testData.ts
+++ playwright-js-pom/utils\testData.ts
@@ -1,4 +1,5 @@
-export const testData = {
-  validUser: { username: 'user', password: 'pass' },
-  invalidUser: { username: 'wrong', password: 'wrong' }
+export const TestData = {
+  baseURL: 'https://invoice-financing-portal.services.dpw.us.virtusa.dev/?tenant=xbank',
+  username: 'peter@nestle.com',
+  password: 'Scb@1234',
 };
