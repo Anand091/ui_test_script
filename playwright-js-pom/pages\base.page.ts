9f00cf0b0cb69ff94aa8994b333fc97d68dd03ff
Anand091 <anandkummari8765@gmail.com>
2025-07-30 01:44:58 +0530

    Add generated Playwright POM test suite

---
+++ playwright-js-pom/pages\base.page.ts
@@ -0,0 +1,10 @@
+import { Page } from '@playwright/test';
+
+export class BasePage {
+  constructor(public page: Page) {
+  }
+
+  async navigate(url: string) {
+    await this.page.goto(url);
+  }
+}
